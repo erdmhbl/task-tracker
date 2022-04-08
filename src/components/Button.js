b8d0fe07ae1d1fd49d88fce8e2116a49b9a87986
erdmhbl <erdmhbl@gmail.com>
2022-04-08 16:53:12 +0300

    v1

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Button = ({ handleClick, color, text }) => {
+  /*    const handleClick = () => {
+        console.log("Click with handle")
+    } */
+  return (
+    <div>
+      <button
+        className="btn"
+        style={{ backgroundColor: color}}
+        onClick={handleClick}
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  const handleClick = () => {
+    console.log("Click with handle")
+}
+  return (
+    <div className="header">
+      {/* <h1>Task Tracker</h1> */}
+      <h1>{title}</h1>
+      <Button handleClick={handleClick} color="purple" text="Show Add Task Bar" />
+    </div>
+  );
+};
+
+// Header.propTypes = {
+//     title: PropTypes.string.isRequired,
+// }; //! string değer istediğimizde..
+
+// Header.defaultProps = {
+//     title: "Task Tracker",
+// }
+
+// title = "Task Default" in props area
+
+export default Header;
